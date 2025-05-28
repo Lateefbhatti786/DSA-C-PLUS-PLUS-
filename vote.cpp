2a98f82bd4d63f44a06beb43998b7b5191ac5e10
Lateef <lateefboss2302@gmail.com>
2025-05-28 19:48:40 +0530

    added name and greetings

--- vote.cpp
+++ vote.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 int main(){
+    string name;
+    cout << "ENTER YOUR NAME : ";
+    cin >> name;
+    cout <<"hello " << name <<endl;
+    cout << "Welcome to the voting eligibility check program!" << endl; 
+
+
     int age;
-    cout<< "ENTER YOUR AGE : ";
+    cout<< "ENTER YOUR AGE  TO CHECK ELIGIBILITY: ";
     cin >> age;
+    
 
     if (age>= 18)
     {
