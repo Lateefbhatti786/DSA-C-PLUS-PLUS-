9805e59b210e7b606609aee95724326428517c39
Lateef <lateefboss2302@gmail.com>
2025-06-21 14:38:00 +0530

    functions

--- functions.cpp
+++ functions.cpp
@@ -157,38 +157,38 @@
 
 // CALCULATE nCr BINOMAIL COEEFICIANT FOR n AND r
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int factorial(int n ){
-    int fact = 1;
-    for (int i = 1 ; i <=n ; i ++){
-        fact*=i;
-    }
-    return fact;
+// int factorial(int n ){
+//     int fact = 1;
+//     for (int i = 1 ; i <=n ; i ++){
+//         fact*=i;
+//     }
+//     return fact;
 
-}
+// }
 
-int nCr(int n , int r ){
-    int fact_n = factorial(n);
-    int fact_r = factorial(r);
-    int fact_n_r = factorial(n-r);
+// int nCr(int n , int r ){
+//     int fact_n = factorial(n);
+//     int fact_r = factorial(r);
+//     int fact_n_r = factorial(n-r);
 
-    return fact_n / (fact_r * fact_n_r);
-}
+//     return fact_n / (fact_r * fact_n_r);
+// }
 
-int main(){
-    int n;
-    cout << "Enter the value of n : " ;
-    cin>> n;
+// int main(){
+//     int n;
+//     cout << "Enter the value of n : " ;
+//     cin>> n;
     
-    int r;
-    cout <<"Enter the value of r : ";
-    cin>> r;
+//     int r;
+//     cout <<"Enter the value of r : ";
+//     cin>> r;
     
-    cout<<"Binomial Coefficiant for "<< n << " and "<< r << " is "<<nCr(n,r)<<endl;
+//     cout<<"Binomial Coefficiant for "<< n << " and "<< r << " is "<<nCr(n,r)<<endl;
 
-    return 0;
+//     return 0;
 
 
-}
+// }
